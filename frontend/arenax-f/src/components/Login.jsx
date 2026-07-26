14b6b8ef470ac1a02ab6ff0214bfb8401a3a2b11
saurabhvarmacoder6 <saurabhkumar9099310496@gamil.com>
2026-07-26 13:27:37 +0530

    some changes

--- frontend/arenax-f/src/components/Login.jsx
+++ frontend/arenax-f/src/components/Login.jsx
@@ -21,6 +21,8 @@ const Login = () => {
     });
 
     const handleSubmit = async () => {
+        console.log(import.meta.env.VITE_API_URL);
+        
         const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
             method: "POST",
             credentials: "include",
