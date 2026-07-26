cc9360d867674d7d925f11324f2f19980d9f5ff7
saurabhvarmacoder6 <saurabhkumar9099310496@gamil.com>
2026-07-26 13:51:42 +0530

    change cookie

--- backend/controllers/auth/loginController.js
+++ backend/controllers/auth/loginController.js
@@ -40,7 +40,7 @@ export const Login = async (req, res) => {
                 res.cookie("token", token, {
                     httpOnly: true,
                     secure: process.env.NODE_ENV === "production",
-                    sameSite: "lax",
+                    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
                     maxAge: 6 * 24 * 60 * 60 * 1000
                 })
                 res.status(200).json({
--- backend/controllers/auth/signupController.js
+++ backend/controllers/auth/signupController.js
@@ -34,7 +34,7 @@ export const signUp = async (req, res) => {
                 res.cookie("token", token, {
                     httpOnly: true,
                     secure: process.env.NODE_ENV === "production",
-                    sameSite: "lax",
+                    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
                     maxAge: 6 * 24 * 60 * 60 * 1000
                 })
                 return res.status(201).json({
