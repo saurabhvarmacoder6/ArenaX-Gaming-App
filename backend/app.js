940ef049a125d8dce762647c0687073ee828d2a0
saurabhvarmacoder6 <saurabhkumar9099310496@gamil.com>
2026-07-26 12:24:11 +0530

    added url in .env

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,12 @@ import authRoutes from "./routes/authRoutes.js"
 import cookieParser from "cookie-parser";
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
